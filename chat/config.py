9dc430960a531173375b778abf5c22bf41ddec8b
FilTheo <theodosiou.filotas@gmail.com>
2025-12-06 12:47:41 +0200

    new models

--- chat/config.py
+++ chat/config.py
@@ -1,7 +1,11 @@
 COUNCIL_MEMBERS = [
-    "openai/gpt-oss-20b:nebius",
-    "moonshotai/Kimi-K2-Thinking:novita",
-    "Qwen/Qwen3-0.6B:fireworks-ai",
-    "meta-llama/Llama-3.1-8B-Instruct:novita",
-    "zai-org/GLM-4.6:novita"
+    "openai/gpt-oss-20b:groq",
+    #"moonshotai/Kimi-K2-Thinking",
+    #"moonshotai/Kimi-Linear-48B-A3B-Instruct",
+    "Qwen/Qwen3-0.6BB",
+    "meta-llama/Llama-3.1-8B-Instruct",
+    "meta-llama/Llama-3.2-3B-Instruct",
+    "google/gemma-3-27b-it"
 ]
+
+ARBITER_MODEL = "google/gemma-3-27b-it"
